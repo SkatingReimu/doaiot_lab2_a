21f1a683f95c10bfdaec145d63fd6faff684bd85
SkatingReimu <130963401+SkatingReimu@users.noreply.github.com>
2023-05-08 15:45:55 +0800

    for lab 3

--- bmi.py
+++ bmi.py
@@ -1,16 +1,10 @@
 def calc_bmi(h,w):
-    print("height = "+str(h))
-    print("weight = "+str(w))
     bmi = w/(h*h)
-    print("your bmi is: "+str(bmi))
     if bmi<18.5:
-        print("you are underweight")
         return -1
     elif bmi>=18.5 and bmi<=25.0:
-        print("your weight is normal")
         return 0
     else:
-        print("you are overweight")
         return 1
 
 
